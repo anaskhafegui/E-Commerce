42e66f95007a66b793ce7990dc666099c9700b62
anaskhafegui <anas.mdhat@gmail.com>
2019-09-24 20:07:59 +0200

    intial-category-produts

---
+++ routes/api.php
@@ -0,0 +1,5 @@
+<?php
+
+
+Route::resource('categories','Categories\CategoryController');
+Route::resource('products','Products\ProductController');
---
+++ tests/Unit/Models/Categories/CategoryTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit\Models\Categories;
+use App\Models\Category;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CategoryTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_it_many_children()
+    {
+        $category = factory(Category::class)->create();
+
+        $category->children()->save(
+            factory(Category::class)->create()
+        );
+
+        $this->assertInstanceOf(Category::class, $category->children->first());
+    }
+}
